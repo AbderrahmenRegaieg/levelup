0519c1880c067baef2d8918ae5d6c2c72382055c
Abderrahmen Regaieg <regaiegabderrahmen0@gmail.com>
2025-07-13 17:02:24 +0100

    changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header";
+import { HeroComponent } from "./hero/hero";
+import {  ContactComponent } from "./contact/contact";
+import { Footer } from './footer/footer';
+import { ClientsComponent } from './clients/clients';
+import { ServicesComponent } from './servises/servises';
+import { TeamComponent } from './team/team';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, HeroComponent,ServicesComponent, TeamComponent,ContactComponent, Footer, ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'LevelupFront';
+  protected title = 'First-App';
 }
+// app.module.ts
+
+
+ 
